91ee097278c3bf9153698b9505d5eb2474e44e07
The oblivions coding <wesvevoormc@gmail.com>
2025-11-05 14:31:25 +0100

    Inital commit 3

--- Main.py
+++ Main.py
@@ -11,15 +11,19 @@ pygame.display.set_caption("Pixel Chance")
 
 class sprite:
     def __init__(self, image, size):
-        self.original_image = pygame.image.load(f"{image}.png")
+        self.original_image = pygame.image.load(f"{image}.png").convert_alpha()
         self.image = pygame.transform.scale_by(self.original_image, size)
+
         self.rect = self.image.get_rect()
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
 
 
 MainButton = {"idle": sprite('MainButtons/Idle', 5),
-              "sprite('MainButtons/Highlighted', 5),
-              sprite('MainButtons/Held', 5)}
-MainButtonType = 0
+              "highlight": sprite('MainButtons/Highlighted', 5),
+              "held": sprite('MainButtons/Held', 5)}
+
+MainButtonType = "idle"
 
 running = True
 while running:
@@ -29,11 +33,16 @@ while running:
     mouseHeld = pygame.mouse.get_pressed()[0]
     mousePos = pygame.mouse.get_pos()
     
-    if MainButton[0].rect.collidepoint(mousePos):
+    if MainButton[MainButtonType].rect.collidepoint(mousePos):
         if mouseHeld:
-            MainButtonType
+            MainButtonType = "held"
+        else:
+            MainButtonType = "highlight"
+    else: MainButtonType = "idle"
 
-    window.blit(MainButton[MainButtonType].image, (0,0))
+    window.blit(MainButton[MainButtonType].image,
+                (width/2+(MainButton[MainButtonType].width/2),
+                 height/2+(MainButton[MainButtonType].height/2)))
 
     pygame.display.flip()
 
