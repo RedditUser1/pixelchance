256b0fc07d98ec11353cccf54d7de616f2b3afd5
RedditUser1 <mastlerd0@gmail.com>
2025-11-05 14:50:55 +0100

    added something

--- Main.py
+++ Main.py
@@ -10,18 +10,28 @@ clock = pygame.time.Clock()
 pygame.display.set_caption("Pixel Chance")
 
 class sprite:
-    def __init__(self, image, size):
+    def __init__(self, image, pos=(0,0), size=5):
         self.original_image = pygame.image.load(f"{image}.png").convert_alpha()
         self.image = pygame.transform.scale_by(self.original_image, size)
 
-        self.rect = self.image.get_rect()
         self.width = self.image.get_width()
         self.height = self.image.get_height()
+        self.x = pos[0]-(self.width)
+        self.y = pos[1]-(self.height)
+        self.pos = (self.x, self.y)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
 
+    def move(self, pos=(0,0)):
+        self.x = pos[0]-(self.width)
+        self.y = pos[1]-(self.height)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
 
-MainButton = {"idle": sprite('MainButtons/Idle', 5),
-              "highlight": sprite('MainButtons/Highlighted', 5),
-              "held": sprite('MainButtons/Held', 5)}
+MainButton = {}
+for name in ["Idle", "Highlight", "Held"]:
+    MainButton[name.lower()] = sprite(f'MainButtons/{name}', (width/2, height/2), 5)
+
 
 MainButtonType = "idle"
 
@@ -32,8 +42,6 @@ while running:
             running = False
     mouseHeld = pygame.mouse.get_pressed()[0]
     mousePos = pygame.mouse.get_pos()
-
-    window.fill(0,0,0)
     
     if MainButton[MainButtonType].rect.collidepoint(mousePos):
         if mouseHeld:
@@ -42,7 +50,8 @@ while running:
             MainButtonType = "highlight"
     else: MainButtonType = "idle"
 
-    window.blit(MainButton[MainButtonType].image, (0,0))
+    window.blit(MainButton[MainButtonType].image, MainButton[MainButtonType].pos)
+
 
     pygame.display.flip()
 
